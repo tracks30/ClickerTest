cca1c49f27110b79b9978f460ca8de976cadf72b
tracks30 <38045404+tracks30@users.noreply.github.com>
2018-04-05 12:01:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var cookies = 0;
 function cookieClick(number){
     cookies = cookies + number;
     document.getElementById("cookies").innerHTML = cookies;
+	ga('send', 'event', 'ClickerTest', 'Click');
 };
 
 var cursors = 0;
@@ -17,6 +18,7 @@ function buyCursor(){
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+	ga('send', 'event', 'ClickerTest', 'CurserBuy');
 };
 
 window.setInterval(function(){
